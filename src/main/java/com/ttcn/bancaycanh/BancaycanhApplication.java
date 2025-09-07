9b1013f0f59cd0c3f14f8877abf702f4909ae345
YuukiDuck <nqduc211223@example.com>
2025-09-07 09:23:44 +0700

    First commit

---
+++ src/main/java/com/ttcn/bancaycanh/BancaycanhApplication.java
@@ -0,0 +1,13 @@
+package com.ttcn.bancaycanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaycanhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancaycanhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttcn/bancaycanh/BancaycanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttcn.bancaycanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancaycanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
